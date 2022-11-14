69630f223b510f4c40f7e82c9a8b8679a365e695
aelassas <akram.elassas@gmail.com>
2022-11-14 12:38:52 +0100

    Update COOCKIES_OPTIONS

--- backend/config/env.config.js
+++ backend/config/env.config.js
@@ -42,7 +42,7 @@ const Env = {
         SHIPPED: 'shipped',
         CANCELLED: 'cancelled'
     },
-    COOCKIES_OPTIONS: { maxAge: 100 * 365 * 24 * 60 * 60, sameSite: true },
+    COOCKIES_OPTIONS: { maxAge: 100 * 365 * 24 * 60 * 60 },
 };
 
 export default Env;
