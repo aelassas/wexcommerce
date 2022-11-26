fe04241ca881413be12dea3d958b97d147da3ebc
aelassas <akram.elassas@gmail.com>
2022-11-26 18:33:24 +0100

    Code refactoring

--- backend/lang/categories.js
+++ backend/lang/categories.js
@@ -5,13 +5,13 @@ export const strings = new LocalizedStrings({
         NEW_CATEGORY: 'Nouvelle Catégorie',
         EMPTY_LIST: 'Pas de catégories',
         DELETE_CATEGORY: 'Êtes-vous sûr de vouloir supprimer cette catégorie ?',
-        CANNOT_DELETE_CATEGORY: 'Cette catégorie ne peut pas être supprimé car il est lié à des vidéos.',
+        CANNOT_DELETE_CATEGORY: 'Cette catégorie ne peut pas être supprimé car il est lié à des produits.',
     },
     en: {
         NEW_CATEGORY: 'New Category',
         EMPTY_LIST: 'No categories',
         DELETE_CATEGORY: 'Are you sure you want to delete this category?',
-        CANNOT_DELETE_CATEGORY: 'This category cannot be deleted because it is related to videos.',
+        CANNOT_DELETE_CATEGORY: 'This category cannot be deleted because it is related to products.',
     }
 });
 
