4cf876ac4191b410e14c033bd38732dcb723eb9e
aelassas <akram.elassas@gmail.com>
2024-10-09 18:01:30 +0100

    Fix orderController.ts

--- api/src/controllers/orderController.ts
+++ api/src/controllers/orderController.ts
@@ -222,15 +222,17 @@ export const checkout = async (req: Request, res: Response) => {
         _order.status = wexcommerceTypes.OrderStatus.Pending
         _order.expireAt = expireAt
 
-        //
-        // Non verified and active users created from checkout with Stripe are temporary
-        // and are automatically deleted if the payment checkout session expires.
-        //
-        expireAt = new Date()
-        expireAt.setSeconds(expireAt.getSeconds() + env.USER_EXPIRE_AT)
+        if (!_user.verified) {
+          //
+          // Non verified and active users created from checkout with Stripe are temporary
+          // and are automatically deleted if the payment checkout session expires.
+          //
+          expireAt = new Date()
+          expireAt.setSeconds(expireAt.getSeconds() + env.USER_EXPIRE_AT)
 
-        _user.expireAt = expireAt
-        await _user.save()
+          _user.expireAt = expireAt
+          await _user.save()
+        }
       }
     }
 
