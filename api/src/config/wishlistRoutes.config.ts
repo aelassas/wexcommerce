d0e4fffe0520b18b1e598fa0c9d6bf4c10ddb671
aelassas <akram.elassas@gmail.com>
2024-10-10 11:21:29 +0100

    Add wishlistRoutes.config.ts

---
+++ api/src/config/wishlistRoutes.config.ts
@@ -0,0 +1,8 @@
+export default {
+    addItem: '/api/add-wishlist-item',
+    deleteItem: '/api/delete-wishlist-item/:wishlist/:product',
+    getWishlist: '/api/wishlist/:id',
+    getWishlistCount: '/api/wishlist-count/:id',
+    getWishlistId: '/api/wishlist-id/:user',
+    clearWishlist: '/api/clear-wishlist/:id',
+}
