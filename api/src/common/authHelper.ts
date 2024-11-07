e27d8b0087b504c113eaf11ecf846f515a2a6b84
aelassas <akram.elassas@gmail.com>
2024-11-07 18:20:29 +0100

    Update authHelper.ts

--- api/src/common/authHelper.ts
+++ api/src/common/authHelper.ts
@@ -61,24 +61,3 @@ export const isBackend = (req: Request): boolean => !!req.headers.origin && help
  * @returns {boolean}
  */
 export const isFrontend = (req: Request): boolean => !!req.headers.origin && helper.trimEnd(req.headers.origin, '/') === helper.trimEnd(env.FRONTEND_HOST, '/')
-
-/**
- * Get authentification cookie name.
- *
- * @param {Request} req
- * @returns {string}
- */
-export const getAuthCookieName = (req: Request): string => {
-  if (isBackend(req)) {
-    // Backend auth cookie name
-    return env.BACKEND_AUTH_COOKIE_NAME
-  }
-
-  if (isFrontend(req)) {
-    // Frontend auth cookie name
-    return env.FRONTEND_AUTH_COOKIE_NAME
-  }
-
-  // Mobile app and unit tests auth header name
-  return env.X_ACCESS_TOKEN
-}
