45db3df1bc8b6b1bd69dd4d3117ac7ed4b38a9a4
aelassas <akram.elassas@gmail.com>
2024-12-28 09:16:19 +0100

    Update ga4.ts

--- frontend/src/common/ga4.ts
+++ frontend/src/common/ga4.ts
@@ -4,10 +4,6 @@ import env from '@/config/env.config'
 const TRACKING_ID = env.GOOGLE_ANALYTICS_ID
 const { isProduction } = env
 
-// export const init = () => ga4.initialize(TRACKING_ID, {
-//   testMode: !isProduction
-// })
-
 export const init = () => {
   if (typeof window === 'undefined') {
     return
@@ -22,12 +18,10 @@ export const init = () => {
 
   window.addEventListener('mousemove', loadAnalyticsScript, { once: true })
   window.addEventListener('touchstart', loadAnalyticsScript, { once: true })
-  window.addEventListener('touchmove', loadAnalyticsScript, { once: true })
-  window.addEventListener('touchend', loadAnalyticsScript, { once: true })
 }
 
 export const sendEvent = (name: string) => ga4.event('screen_view', {
-  app_name: 'ErbilCarRent',
+  app_name: 'wexcommerce',
   screen_name: name,
 })
 
