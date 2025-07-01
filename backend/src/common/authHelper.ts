74739900a4f2a423361323d9ddc3a532feca7ae1
aelassas <akram.elassas@gmail.com>
2025-07-01 16:10:18 +0100

    feat(tests): add comprehensive tests for JWT and reCAPTCHA validation

--- backend/src/common/authHelper.ts
+++ backend/src/common/authHelper.ts
@@ -1,6 +1,8 @@
 import { Request } from 'express'
 import { jwtVerify, SignJWT } from 'jose'
 import bcrypt from 'bcrypt'
+import axios from 'axios'
+import * as wexcommerceTypes from ':wexcommerce-types'
 import * as helper from './helper'
 import * as env from '../config/env.config'
 
@@ -73,4 +75,54 @@ export const isFrontend = (req: Request): boolean => !!req.headers.origin && hel
 export const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt(10)
   return bcrypt.hash(password, salt)
+}
+
+/**
+ * Parse JWT token.
+ *
+ * @param {string} token
+ * @returns {any}
+ */
+export const parseJwt = (token: string) => JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString())
+
+/**
+ * Validate JWT token structure.
+ *
+ * @param {string} token
+ * @returns {boolean}
+ */
+export const validateAccessToken = async (socialSignInType: wexcommerceTypes.SocialSignInType, token: string, email: string) => {
+  if (socialSignInType === wexcommerceTypes.SocialSignInType.Facebook) {
+    try {
+      parseJwt(token)
+      return true
+    } catch {
+      return false
+    }
+  }
+
+  if (socialSignInType === wexcommerceTypes.SocialSignInType.Apple) {
+    try {
+      const res = parseJwt(token)
+      return res.email === email
+    } catch {
+      return false
+    }
+  }
+
+  if (socialSignInType === wexcommerceTypes.SocialSignInType.Google) {
+    try {
+      const res = await axios.get(
+        'https://www.googleapis.com/oauth2/v3/tokeninfo',
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        },
+      )
+      return res.data.email === email
+    } catch {
+      return false
+    }
+  }
+
+  return false
 }
